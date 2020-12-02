81c860e89cce991dd4478ef53488d4e7a1f00aac
John Chase <ratteal@gmail.com>
2020-12-02 11:34:29 +0000

    Group the commands for each project/site.

--- build.sh
+++ build.sh
@@ -16,25 +16,27 @@ mkdir config
 rm -rf srv
 mkdir srv
 
-printf "\nPulling resources required...\n"
+printf "\nClone and copy files for f4rside.com...\n"
 # Checkout a shallow copy of f4rside-site.
 git clone --single-branch --depth 1 https://github.com/RatJuggler/f4rside-site.git src/f4rside-site
+# Copy f4rside-site distribution files.
+cp -r src/f4rside-site/src srv/f4rside.com
+
+printf "\nClone, build and copy files for jurassic-john.site...\n"
 # Checkout a shallow copy of developer-portfolio.
 git clone --single-branch --depth 1 https://github.com/RatJuggler/developer-portfolio.git src/developer-portfolio
-# Checkout a shallow copy of the base Nginx configuration.
-git clone --single-branch --branch production --depth 1 https://github.com/RatJuggler/server-configs-nginx.git src/server-config-nginx
-
-printf "\nBuilding/copying sites to serve...\n"
-# Copy f4rside-site files.
-cp -r src/f4rside-site/src srv/f4rside.com
-# Build and copy developer-portfolio site.
+# Build developer-portfolio site.
 cd src/developer-portfolio || exit
 npm install
 node_modules/.bin/gulp build
 cd ../..
+# Copy developer-portfolio distribution files.
 cp -r src/developer-portfolio/dist srv/jurassic-john.site
 
-printf "\nCopying Nginx configuration...\n"
+printf "\nClone and copy files for Nginx configuration...\n"
+# Checkout a shallow copy of the base Nginx configuration.
+git clone --single-branch --branch production --depth 1 https://github.com/RatJuggler/server-configs-nginx.git src/server-config-nginx
+# Copy the required files.
 cp -r src/server-config-nginx/h5bp config/h5bp
 cp -r nginx/conf.d config/conf.d
 cp src/server-config-nginx/conf.d/.default.conf config/conf.d
