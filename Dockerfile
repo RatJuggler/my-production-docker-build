00a75e140758065dbab254afe941b46351ae7654
John Chase <ratteal@gmail.com>
2020-12-08 17:44:55 +0000

    Start moving the build into the docker file.

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,35 @@
-# Nginx base image.
+# Build any applications requiring Node.
+FROM node:12.20.0 AS builder
+
+# developer-portfolio build.
+
+# Location of source code.
+WORKDIR /developer-portfolio
+# Install dependencies.
+COPY src/developer-portfolio/package.json .
+RUN npm install
+# Copy the sources and run the build.
+COPY src/developer-portfolio .
+RUN node_modules/.bin/gulp build
+
+
+# Create the Nginx application image.
 FROM nginx
-# Nginx configuration.
-COPY config /etc/nginx
+
 # Create a folder to server the site(s) from.
 WORKDIR /srv
-# Copy the site(s) and certificates.
-COPY srv .
+
+# Copy f4rside-site distribution files.
+COPY src/f4rside-site/src/ /srv/f4rside.com/
+
+# Copy developer-portfolio distribution files.
+COPY --from=builder /developer-portfolio/dist/public/ /srv/jurassic-john.site/
+
+# Copy Nginx configuration files.
+COPY config/ /etc/nginx/
+
+# Copy the certificates.
+COPY nginx/certs/ certs/
+
 # Protect any private keys.
 RUN chmod 400 /srv/certs/*.key
--- build.sh
+++ build.sh
@@ -13,42 +13,21 @@ rm -rf src
 mkdir src
 rm -rf config
 mkdir config
-rm -rf srv
-mkdir srv
 
-printf "\nClone and copy files for f4rside.com...\n"
-# Checkout a shallow copy of f4rside-site.
+printf "\nClone files for f4rside.com...\n"
 git clone --single-branch --depth 1 https://github.com/RatJuggler/f4rside-site.git src/f4rside-site
-# Copy f4rside-site distribution files.
-cp -r src/f4rside-site/src srv/f4rside.com
 
-printf "\nClone, build and copy files for jurassic-john.site...\n"
-# Checkout a shallow copy of developer-portfolio.
+printf "\nClone files for jurassic-john.site...\n"
 git clone --single-branch --depth 1 https://github.com/RatJuggler/developer-portfolio.git src/developer-portfolio
-# Build developer-portfolio site.
-cd src/developer-portfolio || exit
-npm install
-node_modules/.bin/gulp build
-cd ../..
-# Copy developer-portfolio distribution files.
-cp -r src/developer-portfolio/dist/public srv/jurassic-john.site
 
 printf "\nClone and copy files for Nginx configuration...\n"
-# Checkout a shallow copy of the base Nginx configuration.
 git clone --single-branch --depth 1 --branch production https://github.com/RatJuggler/server-configs-nginx.git src/server-config-nginx
-# Copy the required files.
 cp -r src/server-config-nginx/h5bp config/h5bp
 cp -r nginx/conf.d config/conf.d
 cp src/server-config-nginx/conf.d/.default.conf config/conf.d
 cp src/server-config-nginx/conf.d/no-ssl.default.conf config/conf.d
 cp src/server-config-nginx/mime.types config
 cp src/server-config-nginx/nginx.conf config
-
-printf "\nCopying certificates...\n"
-cp -r nginx/certs srv/certs
-
-printf "\nRemoving sources after build and copy...\n"
-rm -rf src
 
 printf "\nRunning Docker build!\n"
 docker build -t "$1" .
