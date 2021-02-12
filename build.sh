ddcb295ee20e0a99823957e3286ce898c6f8e0fd
John Chase <ratteal@gmail.com>
2021-02-12 17:18:02 +0000

    Simplify builds with a function.

--- build.sh
+++ build.sh
@@ -1,15 +1,37 @@
 #!/bin/bash
 
+function checkout_and_build() {
+  REPO=$1
+  # Checkout the bare source from repo, no history
+  git clone --single-branch --no-tags --depth 1 https://github.com/RatJuggler/$REPO.git src/$REPO
+  # Make repo the CWD.
+  cd src/$REPO || exit
+  # env_file entries must exist for the compose file to be validated so we create a dummy one for building.
+  touch $REPO.env
+  # Do the build.
+  docker-compose -f docker-compose.yml build --pull --build-arg BUILD_TAG="$BUILD_TAG"
+  # Only do a push if a registry and an id have been set.
+  if [[ -n "$DOCKER_REGISTRY" && -n "$DOCKER_ID" ]]; then
+    docker-compose -f docker-compose.yml push
+  fi
+  # Restore previous CWD.
+  cd ../.. || exit
+}
+
 printf "My Production Docker Builder\n"
 printf "============================\n\n"
+
+export DOCKER_REGISTRY=""
+export DOCKER_ID=""
+export IMAGE_TAG="latest"
 
 ARCH=$(uname -m)
 case "$ARCH" in
   x86_64)
-    export BUILD_TAG="latest"
+    export BUILD_TAG="local"
     ;;
   armv7l)
-    export BUILD_TAG="latest"
+    export BUILD_TAG="local"
     ;;
   *)
     printf "Unexpected architecture '%s' encountered!\n" "$ARCH"
@@ -24,50 +46,11 @@ printf "\nClearing down any previous builds...\n"
 rm -rf src
 mkdir src
 
-# Build for sync-gandi-dns site.
-
-git clone --single-branch --no-tags --depth 1 https://github.com/RatJuggler/sync-gandi-dns.git src/sync-gandi-dns
-
-cd src/sync-gandi-dns || exit
-docker-compose -f docker-compose.yml build --pull --build-arg BUILD_TAG="$BUILD_TAG"
-docker-compose -f docker-compose.yml push
-cd ../.. || exit
-
-# Build for guinea-bot site.
-
-git clone --single-branch --no-tags --depth 1 https://github.com/RatJuggler/guinea-bot.git src/guinea-bot
-
-cd src/guinea-bot || exit
-docker-compose -f docker-compose.yml build --pull --build-arg BUILD_TAG="$BUILD_TAG"
-docker-compose -f docker-compose.yml push
-cd ../.. || exit
-
-# Build for dinosauria-bot site.
-
-git clone --single-branch --no-tags --depth 1 https://github.com/RatJuggler/dinosauria-bot.git src/dinosauria-bot
-
-cd src/dinosauria-bot || exit
-docker-compose -f docker-compose.yml build --pull --build-arg BUILD_TAG="$BUILD_TAG"
-docker-compose -f docker-compose.yml push
-cd ../.. || exit
-
-# Build for f4rside.com site.
-
-git clone --single-branch --no-tags --depth 1 https://github.com/RatJuggler/f4rside-site.git src/f4rside-site
-
-cd src/f4rside-site || exit
-docker-compose -f docker-compose.yml build --pull --build-arg BUILD_TAG="$BUILD_TAG"
-docker-compose -f docker-compose.yml push
-cd ../.. || exit
-
-# Build for jurassic-john.site site.
-
-git clone --single-branch --no-tags --depth 1 https://github.com/RatJuggler/developer-portfolio.git src/developer-portfolio
-
-cd src/developer-portfolio || exit
-docker-compose -f docker-compose.yml build --pull --build-arg BUILD_TAG="$BUILD_TAG"
-docker-compose -f docker-compose.yml push
-cd ../.. || exit
+checkout_and_build sync-gandi-dns
+checkout_and_build guinea-bot
+checkout_and_build dinosauria-bot
+checkout_and_build f4rside-site
+checkout_and_build developer-portfolio
 
 # Build for the ingress proxy.
 # IMPORTANT: We must *NOT* push the ingress proxy image to a public repository whilst it contains our certificate keys!
