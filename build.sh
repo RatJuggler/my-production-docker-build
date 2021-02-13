635c7cf90f7d316c11d877eb953534f1bac40091
John Chase <ratteal@gmail.com>
2021-02-13 12:55:36 +0000

    Clarify use of default options.

--- build.sh
+++ build.sh
@@ -28,26 +28,49 @@ printf "============================\n\n"
 
 while getopts :i:r:t: OPTION
 do
-    case "${OPTION}" in
-        i)
-          export DOCKER_ID=${OPTARG}
-          ;;
-        r)
-          export DOCKER_REGISTRY=${OPTARG}
-          ;;
-        t)
-          export IMAGE_TAG=${OPTARG}
-          ;;
-        ?)
-          printf "Usage: %s -i DOCKER_ID -r DOCKER_REGISTRY -t IMAGE_TAG\n" "$0"
-          exit 1
-          ;;
-    esac
+  case "${OPTION}" in
+    i)
+      export DOCKER_ID=${OPTARG}
+      ;;
+    r)
+      export DOCKER_REGISTRY=${OPTARG}
+      ;;
+    t)
+      export IMAGE_TAG=${OPTARG}
+      ;;
+    ?)
+      printf "Usage: %s -i DOCKER_ID -r DOCKER_REGISTRY -t IMAGE_TAG\n" "$0"
+      exit 1
+      ;;
+  esac
 done
 
-printf "Docker Registry         : %s\n" "$DOCKER_REGISTRY"
-printf "Docker Id               : %s\n" "$DOCKER_ID"
-printf "Image Tag               : %s\n" "$IMAGE_TAG"
+if [[ -n "$DOCKER_REGISTRY" && -z "$DOCKER_ID" ]]; then
+  printf "Error: Docker Id must also be set when specifying a registry!\n"
+  exit 1
+fi
+
+if [[ -z "$DOCKER_REGISTRY" ]]; then
+  printf -v REGISTRY_USED "<Not set, local image only>"
+else
+  printf -v REGISTRY_USED "%s" "$DOCKER_REGISTRY"
+fi
+
+if [[ -z "$DOCKER_ID" ]]; then
+  printf -v ID_USED "<Not set, local image only>"
+else
+  printf -v ID_USED "%s" "$DOCKER_ID"
+fi
+
+if [[ -z "$IMAGE_TAG" ]]; then
+  printf -v TAG_USED "<Not set, will default to 'latest'>"
+else
+  printf -v TAG_USED "%s" "$IMAGE_TAG"
+fi
+
+printf "Docker Registry         : %s\n" "$REGISTRY_USED"
+printf "Docker Id               : %s\n" "$ID_USED"
+printf "Image Tag               : %s\n" "$TAG_USED"
 
 ARCH=$(uname -m)
 case "$ARCH" in
@@ -80,12 +103,12 @@ checkout_and_build developer-portfolio
 # IMPORTANT: We must *NOT* push the ingress proxy image to a public repository whilst it contains our certificate keys!
 
 docker-compose -f docker-compose.yml build \
-    --build-arg DOCKER_REGISTRY="$DOCKER_REGISTRY" \
-    --build-arg DOCKER_ID="$DOCKER_ID" \
-    --build-arg BUILD_TAG="$BUILD_TAG"
+  --build-arg DOCKER_REGISTRY="$DOCKER_REGISTRY" \
+  --build-arg DOCKER_ID="$DOCKER_ID" \
+  --build-arg BUILD_TAG="$BUILD_TAG"
 docker-compose -f docker-compose-production.yml build \
-    --build-arg DOCKER_REGISTRY="$DOCKER_REGISTRY" \
-    --build-arg DOCKER_ID="$DOCKER_ID" \
-    --build-arg BUILD_TAG="$BUILD_TAG"
+  --build-arg DOCKER_REGISTRY="$DOCKER_REGISTRY" \
+  --build-arg DOCKER_ID="$DOCKER_ID" \
+  --build-arg BUILD_TAG="$BUILD_TAG"
 
 printf "\nAll Done!\n"
