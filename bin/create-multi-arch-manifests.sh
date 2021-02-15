3fcc5798e1f070b84d4c1c0a66008e3bf540fa4e
John Chase <ratteal@gmail.com>
2021-02-15 16:14:20 +0000

    Builders will need to be multi-arch.

--- bin/create-multi-arch-manifests.sh
+++ bin/create-multi-arch-manifests.sh
@@ -5,8 +5,12 @@
 ./bin/push-manifest.sh -g docker.io -p johnchase -i dinosauria-bot
 ./bin/push-manifest.sh -g docker.io -p johnchase -i f4rside-site
 
+./bin/push-manifest.sh -g docker.io -p johnchase -i builder-share-resources
+./bin/push-manifest.sh -g docker.io -p johnchase -i builder-portfolio-simple
 ./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-simple
+./bin/push-manifest.sh -g docker.io -p johnchase -i builder-portfolio-sql
 ./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-sql
+./bin/push-manifest.sh -g docker.io -p johnchase -i builder-portfolio-combined
 ./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-template
 ./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-static
 ./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-site
