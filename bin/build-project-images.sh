9308fce5bbf2176c3833d448aae54c9788de9848
John Chase <ratteal@gmail.com>
2021-02-15 15:25:23 +0000

    Fix script references.

--- bin/build-project-images.sh
+++ bin/build-project-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/sync-gandi-dns.git
-./build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/guinea-bot.git
-./build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/dinosauria-bot.git
-./build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/f4rside-site.git
-./build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/developer-portfolio.git
+./bin/build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/sync-gandi-dns
+./bin/build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/guinea-bot
+./bin/build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/dinosauria-bot
+./bin/build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/f4rside-site
+./bin/build.sh -m -g docker.io -p johnchase -u https://github.com/RatJuggler/developer-portfolio
--- bin/create-multi-arch-manifests.sh
+++ bin/create-multi-arch-manifests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-./push-manifest.sh -g docker.io -p johnchase -i sync-gandi-dns
-./push-manifest.sh -g docker.io -p johnchase -i guinea-bot
-./push-manifest.sh -g docker.io -p johnchase -i dinosauria-bot
-./push-manifest.sh -g docker.io -p johnchase -i f4rside-site
+./bin/push-manifest.sh -g docker.io -p johnchase -i sync-gandi-dns
+./bin/push-manifest.sh -g docker.io -p johnchase -i guinea-bot
+./bin/push-manifest.sh -g docker.io -p johnchase -i dinosauria-bot
+./bin/push-manifest.sh -g docker.io -p johnchase -i f4rside-site
 
-./push-manifest.sh -g docker.io -p johnchase -i portfolio-simple
-./push-manifest.sh -g docker.io -p johnchase -i portfolio-sql
-./push-manifest.sh -g docker.io -p johnchase -i portfolio-template
-./push-manifest.sh -g docker.io -p johnchase -i portfolio-static
-./push-manifest.sh -g docker.io -p johnchase -i portfolio-site
+./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-simple
+./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-sql
+./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-template
+./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-static
+./bin/push-manifest.sh -g docker.io -p johnchase -i portfolio-site
