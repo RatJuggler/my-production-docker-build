8d35b1d2de02f5015bcd6cc1d6c2c1b3109a2849
John Chase <ratteal@gmail.com>
2021-02-16 19:16:59 +0000

    Use compose option to target java builds.

--- bin/build-project-images.sh
+++ bin/build-project-images.sh
@@ -4,4 +4,6 @@
 ./bin/build.sh -m -p johnchase -u https://github.com/RatJuggler/guinea-bot
 ./bin/build.sh -m -p johnchase -u https://github.com/RatJuggler/dinosauria-bot
 ./bin/build.sh -m -p johnchase -u https://github.com/RatJuggler/f4rside-site
+
+./bin/build.sh -m -p johnchase -u https://github.com/RatJuggler/developer-portfolio -c docker-compose-builders.yml
 ./bin/build.sh -m -p johnchase -u https://github.com/RatJuggler/developer-portfolio
--- bin/buildx-project-images.sh
+++ bin/buildx-project-images.sh
@@ -4,4 +4,6 @@
 ./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/guinea-bot
 ./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/dinosauria-bot
 ./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/f4rside-site
-./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/developer-portfolio
+
+#./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/developer-portfolio -c docker-compose-builders.yml
+#./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/developer-portfolio
