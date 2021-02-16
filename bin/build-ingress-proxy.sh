71884601ef82c107fd839b8650e52be44cd6a78d
John Chase <ratteal@gmail.com>
2021-02-16 09:13:08 +0000

    Build and push proxy images.

--- bin/build-ingress-proxy.sh
+++ bin/build-ingress-proxy.sh
@@ -1,17 +1,18 @@
 # Build for the ingress proxy.
-# IMPORTANT: We must *NOT* push the ingress proxy image to a public repository whilst it contains our certificate keys!
 
 export REGISTRY="docker.io/"
 export REPOSITORY="johnchase/"
-export IMAGE_TAG="latest"
+export IMAGE_TAG="linux-amd64"
 export BUILD_TAG="local"
 
 docker-compose -f ./docker/ingress/test/docker-compose.yml build \
   --build-arg REGISTRY="$REGISTRY" \
   --build-arg REPOSITORY="$REPOSITORY" \
   --build-arg BUILD_TAG="$BUILD_TAG"
+docker-compose -f ./docker/ingress/test/docker-compose.yml push
 
 docker-compose -f ./docker/ingress/production/docker-compose.yml build \
   --build-arg REGISTRY="$REGISTRY" \
   --build-arg REPOSITORY="$REPOSITORY" \
   --build-arg BUILD_TAG="$BUILD_TAG"
+docker-compose -f ./docker/ingress/production/docker-compose.yml push
