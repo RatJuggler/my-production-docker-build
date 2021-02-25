a4e1448e504e19db61686115afd578af7019f9d5
John Chase <ratteal@gmail.com>
2021-02-25 16:45:48 +0000

    Comment on buildx issue.

--- bin/buildx-project-images.sh
+++ bin/buildx-project-images.sh
@@ -5,6 +5,9 @@
 ./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/dinosauria-bot
 ./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/f4rside-site
 
+# The following image builds are returning an error related to the libseccomp2 issue:
+# OpenJDK Server VM warning: No monotonic clock was available - timed services may be adversely affected if the time-of-day clock changes
+
 ./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/developer-portfolio -c docker-compose-shared.yml
 
 ./bin/buildx.sh -p johnchase -u https://github.com/RatJuggler/developer-portfolio -c docker-compose-builders.yml
